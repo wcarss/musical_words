ae02fd494cf2b51d8a29cd7459c70b3d4a8bf7ee
Wyatt Carss <carss.w@gmail.com>
2014-05-21 11:43:44 -0400

    Show articles and wire up links throughout application.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,14 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
